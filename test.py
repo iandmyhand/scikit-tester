02b82749bdd32ad5f0fa389ff08d0674b3b0a368
SeomGi, Han <iandmyhand@gmail.com>
2017-04-13 22:12:35 +0900

    Add a python file to test scikit.

---
+++ test.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+from IPython.display import display
+
+import logging
+from core.logger import set_logger
+
+logger = logging.getLogger('scikit-tester')
+
+
+def learn():
+    set_logger()
+
+    
+
+
+if '__main__' == __name__:
+    learn()
